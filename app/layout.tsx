cdc7d2542561940af43ca37cdbc2e518b031e792
Guarav Khaire <gauravgkhaire@gmail.com>
2024-12-20 17:22:29 +0530

    now

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -23,16 +22,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        variables: { colorPrimary: "#624cf5" },
-      }}
-    >
-      <html suppressHydrationWarning lang="en">
-        <body className={cn("font-IBMPlex antialiased ", IBMPlex.variable)}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    // <ClerkProvider
+    //   appearance={{
+    //     variables: { colorPrimary: "#624cf5" },
+    //   }}
+    // >
+    <html suppressHydrationWarning lang="en">
+      <body className={cn("font-IBMPlex antialiased ", IBMPlex.variable)}>
+        {children}
+      </body>
+    </html>
+    // </ClerkProvider>
   );
 }
