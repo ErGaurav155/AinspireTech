b157f7aac7a27467ba51cf3b909e593ed3e0c79c
Guarav Khaire <gauravgkhaire@gmail.com>
2025-01-14 18:45:01 +0530

    again

--- app/(root)/ChatBots/page.tsx
+++ app/(root)/ChatBots/page.tsx
@@ -5,11 +5,6 @@ import ChatBot from "@/components/shared/ChatBot";
 import React from "react";
 
 const ChatBots = () => {
-  const userId = new URLSearchParams(window.location.search).get("userId");
-  const agentId = new URLSearchParams(window.location.search).get("agentId");
-  if (!userId || !agentId) {
-    return;
-  }
   return <AibotCollapse />;
 };
 
