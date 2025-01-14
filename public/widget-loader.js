7d6bcace04dcb3b67de9a1ccf1c49960e0f81818
Guarav Khaire <gauravgkhaire@gmail.com>
2025-01-14 17:42:21 +0530

    make it fast

--- public/widget-loader.js
+++ public/widget-loader.js
@@ -1,27 +1,27 @@
 (async function () {
-  const urlParams = new URLSearchParams(window.location.search);
-  const agentId = urlParams.get("agentId");
-  const userId = urlParams.get("userId");
-  const isValid = await fetch("https://your-domain.com/api/validate-widget", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ agentId, userId }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      // Check if the response is valid
-      return data.isValid === "true"; // Check if isValid is 'true'
-    })
-    .catch((error) => {
-      console.error("Error validating the subscription:", error);
-      return false; // If there's an error, return false
-    });
-  if (!isValid) {
-    console.error("Invalid agentId or userId. Widget will not load.");
-    return;
-  }
+  // const urlParams = new URLSearchParams(window.location.search);
+  // const agentId = urlParams.get("agentId");
+  // const userId = urlParams.get("userId");
+  // const isValid = await fetch("https://your-domain.com/api/validate-widget", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({ agentId, userId }),
+  // })
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     // Check if the response is valid
+  //     return data.isValid === "true"; // Check if isValid is 'true'
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error validating the subscription:", error);
+  //     return false; // If there's an error, return false
+  //   });
+  // if (!isValid) {
+  //   console.error("Invalid agentId or userId. Widget will not load.");
+  //   return;
+  // }
 
   const loadScript = (src) => {
     return new Promise((resolve, reject) => {
