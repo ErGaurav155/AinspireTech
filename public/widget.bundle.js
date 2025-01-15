b37c8d82a4248b519162ffef73c661f320650961
Guarav Khaire <gauravgkhaire@gmail.com>
2025-01-15 18:29:19 +0530

    set

--- public/widget.bundle.js
+++ public/widget.bundle.js
@@ -29,8 +29,7 @@
       // Create and append the iframe
       const iframe = document.createElement("iframe");
       iframe.src = `https://ainspire-tech.vercel.app/ChatBots?userId=${userId}&agentId=${agentId}`;
-      iframe.className =
-        "h-auto w-auto flex flex-col fixed bottom-4 right-4 z-999 ";
+      iframe.className = "h-auto w-auto flex flex-col z-50";
 
       container.appendChild(iframe);
     },
