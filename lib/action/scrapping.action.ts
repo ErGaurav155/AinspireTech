f2acf5a44a68722f94406eef18c08cbc4e13b095
Guarav Khaire <gauravgkhaire@gmail.com>
2025-01-20 16:22:56 +0530

    now

--- lib/action/scrapping.action.ts
+++ lib/action/scrapping.action.ts
@@ -1,6 +1,13 @@
 "use server";
-import puppeteer from "puppeteer";
 import chromium from "@sparticuz/chromium";
+
+// Conditionally import puppeteer or puppeteer-core based on the environment
+let puppeteer;
+if (process.env.NODE_ENV === "development") {
+  puppeteer = require("puppeteer");
+} else {
+  puppeteer = require("puppeteer-core");
+}
 
 export const scrapePage = async (url: string) => {
   let browser = null;
@@ -15,18 +22,21 @@ export const scrapePage = async (url: string) => {
   } else if (process.env.NODE_ENV === "production") {
     console.log("Production browser: ");
     browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      headless: true,
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath:
+        (await chromium.executablePath()) ||
+        "/opt/nodejs/node_modules/@sparticuz/chromium/bin",
+      headless: chromium.headless,
     });
   }
 
   if (!browser) {
-    console.log("no browser");
     return;
   }
-  console.log(browser);
+
   const page = await browser.newPage();
-  console.log(page);
+
   // Navigate to the page and wait for the DOM to load
   await page.goto(url, { waitUntil: "domcontentloaded" });
 
@@ -36,17 +46,19 @@ export const scrapePage = async (url: string) => {
   const description = descriptionElement
     ? await descriptionElement
         .getProperty("content")
-        .then((content) => content.jsonValue())
+        .then((content: any) => content.jsonValue())
     : null;
 
   const headingElements = await page.$$("h1, h2, h3");
   const headings = [];
   for (let element of headingElements) {
-    const text = await element.evaluate((el) => el.textContent?.trim() || "");
+    const text = await element.evaluate(
+      (el: any) => el.textContent?.trim() || ""
+    );
     headings.push(text);
   }
 
-  const content = await page.$eval("body", (body) => body.innerText);
+  const content = await page.$eval("body", (body: any) => body.innerText);
 
   // Close the browser once scraping is done
   await browser.close();
