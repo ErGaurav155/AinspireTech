a2afcc14b4157762e0e1492ae60d9283b5eee5f7
Guarav Khaire <gauravgkhaire@gmail.com>
2025-01-20 16:35:35 +0530

    now

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "oaidalleapiprodscus.blob.core.windows.net",
-        port: "",
-      },
-    ],
+  experimental: {
+    serverComponentsExternalPackages: ["puppeteer-core"],
   },
 };
 
