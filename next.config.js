71802a36c187634bcd53566b3e835ae5963c56b7
Guarav Khaire <gauravgkhaire@gmail.com>
2025-01-22 19:17:31 +0530

    abhi

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const path = require("path");
 
-module.exports = {
-  output: "standalone",
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Add the package name to the externals array
+    config.externals = [...config.externals, "chrome-aws-lambda"];
+
+    return config;
+  },
 };
+
+export default nextConfig;
