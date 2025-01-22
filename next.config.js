53d6603ce4180b71295c810d26a28ecbea8e302f
Guarav Khaire <gauravgkhaire@gmail.com>
2025-01-22 18:20:50 +0530

    now

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-const path = require("path");
-
 module.exports = {
-  webpack(config, { isServer }) {
-    // Add custom webpack configuration only for the server-side (because the error occurs on the server side)
-    if (isServer) {
-      config.module.rules.push({
-        test: /\.node$/,
-        use: "null-loader",
-        include: [path.resolve(__dirname, "node_modules/chrome-aws-lambda")],
-      });
-    }
-
-    return config;
-  },
+  output: "standalone",
 };
