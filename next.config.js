8886fb73b597e883020a468ae0f6253db9862231
Guarav Khaire <gauravgkhaire@gmail.com>
2025-07-10 21:03:16 +0530

    push

--- next.config.js
+++ next.config.js
@@ -8,6 +8,24 @@ module.exports = {
           { key: "Access-Control-Allow-Methods", value: "GET" },
         ],
       },
+      {
+        source: "/api/embed/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+        ],
+      },
     ];
   },
   images: {
