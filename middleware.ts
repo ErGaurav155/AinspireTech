3ae17268ae9ae0504fd5bc6b795d05a7c6100a30
Guarav Khaire <gauravgkhaire@gmail.com>
2025-07-10 21:16:20 +0530

    abb

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,42 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
 
-export default authMiddleware({
-  ignoredRoutes: [
-    "/",
-    "/web",
-    "/insta",
-    "/Aboutus",
-    "/web/pricing",
-    "/contactUs",
-    "/OurService",
-    "/Review",
-    "/web/product/:path*",
-    "/new/product",
-    "/api/webhooks/clerk",
-    "/api/embed/chatbot",
-    "/api/embed/conversation",
-    "/api/embed/webQuestion",
-    "/privacy-policy",
-    "/TermsandCondition",
-  ],
-});
+const allowedOrigins = ["*"];
+
+const corsOptions = {
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export function middleware(request: NextRequest) {
+  // Check the origin from the request
+  const origin = request.headers.get("origin") ?? "";
+  const isAllowedOrigin = allowedOrigins.includes(origin);
+
+  // Handle preflighted requests
+  const isPreflight = request.method === "OPTIONS";
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
+      ...corsOptions,
+    };
+    return NextResponse.json({}, { headers: preflightHeaders });
+  }
+
+  // Handle simple requests
+  const response = NextResponse.next();
+
+  if (isAllowedOrigin) {
+    response.headers.set("Access-Control-Allow-Origin", origin);
+  }
+
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
+
+  return response;
+}
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: "/api/:path*",
 };
--- next.config.js
+++ next.config.js
@@ -8,24 +8,6 @@ module.exports = {
           { key: "Access-Control-Allow-Methods", value: "GET" },
         ],
       },
-      {
-        source: "/api/embed/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-        ],
-      },
     ];
   },
   images: {
