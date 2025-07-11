33c27b46744640d9800efadff82bed9b57f311b2
Guarav Khaire <gauravgkhaire@gmail.com>
2025-07-11 13:18:54 +0530

    abb

--- next.config.js
+++ next.config.js
@@ -2,20 +2,10 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/api/embed/:path*",
+        source: "/api/scrape-anu",
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Set your origin
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
         ],
       },
     ];
